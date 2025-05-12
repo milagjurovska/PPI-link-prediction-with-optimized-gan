4f7fc1372876996714f1b86a95a33b6242a732d2
Mila Gjurovska <milagjurovska7@gmail.com>
2025-05-12 23:16:50 +0200

    update gcn

--- codes/gcn.py
+++ codes/gcn.py
@@ -1,8 +1,9 @@
+from sklearn.metrics import f1_score, roc_auc_score
 from data_processing import *
 import torch
 from torch_geometric.nn import GCNConv
 import torch.optim as optim
-
+import numpy as np
 
 def decode(z, edge_label_index):
     src = z[edge_label_index[0]]
@@ -25,7 +26,7 @@ model = GCNLinkPredictor(in_channels=5, hidden_channels=256)
 optimizer = optim.Adam(model.parameters(), lr=0.01)
 
 
-def train():
+def GCNtrain():
     model.train()
     z = model.encode(train_data.x, train_data.edge_index)
     optimizer.zero_grad()
@@ -49,9 +50,41 @@ def train():
 
 
 @torch.no_grad()
-def test(data):
+def GCNtest(data):
     model.eval()
     z = model.encode(data.x, data.edge_index)
     scores = decode(z, data.edge_label_index)
     probs = torch.sigmoid(scores)
     return probs.cpu().numpy()
+
+def find_optimal_threshold(labels, probs):
+    if torch.is_tensor(probs):
+        probs = probs.cpu().numpy()
+    if torch.is_tensor(labels):
+        labels = labels.cpu().numpy()
+
+    thresholds = np.linspace(0, 1, 100)
+    best_f1 = 0
+    best_thresh = 0.5
+    for thresh in thresholds:
+        preds = (probs > thresh).astype(int)
+        f1 = f1_score(labels, preds)
+        if f1 > best_f1:
+            best_f1 = f1
+            best_thresh = thresh
+    return best_thresh
+
+
+def evaluate_model(test_probs, test_labels):
+    if torch.is_tensor(test_probs):
+        test_probs = test_probs.cpu().numpy()
+    if torch.is_tensor(test_labels):
+        test_labels = test_labels.cpu().numpy()
+
+    auc = roc_auc_score(test_labels, test_probs)
+
+    optimal_threshold = find_optimal_threshold(test_labels, test_probs)
+    preds = (test_probs > optimal_threshold).astype(int)
+    f1 = f1_score(test_labels, preds)
+
+    return auc, f1, optimal_threshold
